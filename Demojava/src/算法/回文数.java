ef25f08b62d37214f86a3992efb0e4853c03472d
ZW-Hao <894557678@qq.com>
2020-12-06 17:07:52 +0800

    改进回文数算法

--- Demojava/src/算法/回文数.java
+++ Demojava/src/算法/回文数.java
@@ -5,7 +5,7 @@ public class 回文数 {
         System.out.println(isPalindrome(1211));
     }
 
-    public static boolean isPalindrome(int x) {
+    /*public static boolean isPalindrome(int x) {
         String s=""+x;
         String[] split = s.split("");
         int low=0;
@@ -18,5 +18,23 @@ public class 回文数 {
             high--;
         }
         return true;
+    }*/
+    //不转换为字符串的写法
+    public static boolean isPalindrome(int x){
+        //负数一定不是回文数
+        if(x<0){
+            return false;
+        }
+        //计算出倒着读出的数的值，与x进行比较
+        int y=x;
+        int num=0;
+        while(y!=0){
+            num=num*10+y%10;
+            y=y/10;
+        }
+
+
+        return num==x;
+
     }
 }
