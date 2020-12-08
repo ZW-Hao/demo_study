39ad4cee3d77cf80f15ced3896d2146e23a87793
ZW-Hao <894557678@qq.com>
2020-12-08 09:17:21 +0800

    提交建立数据库表代码

--- 数据库期末作业/addDate.sql
+++ 数据库期末作业/addDate.sql
@@ -16,6 +16,7 @@
 --     supplierId 
 -- )
 -- values(2,'mate40Pro','new 5Gphone',1,8999,1);
+
 -- insert into productTable(
 --     productId ,
 --     productName ,
@@ -25,6 +26,36 @@
 --     supplierId 
 -- )
 -- values(3,'P40Pro','new 4Gphone',1,5499,1);
+
+-- insert into productTable(
+--     productId ,
+--     productName ,
+--     productInformation ,
+--     categoryId ,
+--     price ,
+--     supplierId 
+-- )
+-- values(4,'P40','new 4Gphone',1,4499,1);
+
+-- insert into productTable(
+--     productId ,
+--     productName ,
+--     productInformation ,
+--     categoryId ,
+--     price ,
+--     supplierId 
+-- )
+-- values(5,'matebook14','new computer',2,5299,2);
+
+-- insert into productTable(
+--     productId ,
+--     productName ,
+--     productInformation ,
+--     categoryId ,
+--     price ,
+--     supplierId 
+-- )
+-- values(6,'matebookPro','new computer',2,9999,2);
 
 -- insert into userTable(
 --      userId ,
@@ -59,13 +90,79 @@
 -- )
 -- values(125,8976234,'zhangwei',18,'man','beijing','12800848906');
 
-insert into userTable(
-     userId ,
-    userPassword ,
-    userName ,
-    userAge ,
-    userSex ,
-    userAddress ,
-    userPhone 
+-- insert into userTable(
+--      userId ,
+--     userPassword ,
+--     userName ,
+--     userAge ,
+--     userSex ,
+--     userAddress ,
+--     userPhone 
+-- )
+-- values(126,6666,'xiaomei',24,'woman','shanghai','13884758899');
+
+-- insert into userTable(
+--     userId ,
+--     userPassword ,
+--     userName ,
+--     userAge ,
+--     userSex ,
+--     userAddress ,
+--     userPhone 
+-- )
+-- values(127,9999,'xiaohong',33,'woman','tianjin','16577679900');
+
+
+-- insert into orders(
+--     orderId ,
+--     productId ,
+--     productName,
+--     userId ,
+--     userName ,
+--     orderTime ,
+--     orderAmount
+-- )
+-- values(1,1,'mate40',123,'zhangsan','11/30',6999);
+
+
+insert into supplier(
+    supplierId ,
+    supplierName ,
+    phone ,
+    supplierAddress 
 )
-values(126,6666,'xiaomei',24,'woman','shanghai','13884758899');
+values(1,'phoneSupplier','4770699','shenzhen');
+
+insert into supplier(
+    supplierId ,
+    supplierName ,
+    phone ,
+    supplierAddress
+)
+values(2,'computerSupplier','4550019','guangzhou');
+
+insert into supplier(
+    supplierId ,
+    supplierName ,
+    phone ,
+    supplierAddress 
+)
+values(3,'padSupplier','4230999','shanghai');
+
+insert into productSort(
+    sortId ,
+    sortName
+)
+values(1,'Phone');
+
+insert into productSort(
+    sortId ,
+    sortName
+)
+values(2,'Computer');
+
+insert into productSort(
+    sortId ,
+    sortName
+)
+values(3,'Pad');
